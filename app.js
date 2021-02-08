88d23cf498a754a429f2442eca5e434fc69ac708
toriqul <toriqul.stu@gmail.com>
2021-02-09 03:02:54 +0600

    second

--- app.js
+++ app.js
@@ -1,34 +1,52 @@
 const takeData = async() => {
-
+    const foodDetails = document.getElementById("foodDetails");
+    foodDetails.innerHTML = '';
     const foodName = document.getElementById("search").value;
-    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`);
+    console.log(foodName.length != 1);
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`;
+    const response = await fetch(url);
     const data = await response.json();
     displayFoods(data);
 };
-
 // After Search
 const displayFoods = (data) => {
     const meals = data.meals;
     const cardGroup = document.getElementById("cardGroup");
+    cardGroup.innerHTML = "";
+
     meals.forEach(element => {
+
+        // const cardDiv = document.createElement("div");
+        // cardDiv.className = "col";
+        // cardDiv.innerHTML = `
+        //         <div onclick="displayFoodDetails(${element.idMeal})" class="foodCard  card  text-center">
+        //             <img src="${element.strMealThumb}" class="image card-img-top" alt="...">
+        //             <div class="card-body">
+        //             <h5 class="card-title">${element.strMeal}</h5>
+        //             </div>
+        //         </div>
+        //     `;
+        // cardGroup.appendChild(cardDiv);
+
+
         const cardDiv = document.createElement("div");
         cardDiv.className = "col";
-
         cardDiv.innerHTML = `
-        <div onclick="displayFoodDetails(${element.idMeal})" class="foodCard  card  text-center">
-            <img src="${element.strMealThumb}" class="image card-img-top" alt="...">
-            <div class="card-body">
-            <h5 class="card-title">${element.strMeal}</h5>
+            <div onclick="displayFoodDetails(${element.idMeal})" class="foodCard  card  text-center">
+                <img src="${element.strMealThumb}" class="image card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">${element.strMeal}</h5>
+                </div>
             </div>
-        </div>
-    `;
+        `;
         cardGroup.appendChild(cardDiv);
     });
     document.getElementById("search").value = "";
 };
 
 const displayFoodDetails = async name => {
-    const url = await `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${name}`;
+
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${name}`;
     const response = await fetch(url);
     const data = await response.json()
     renderFoodInfo(data.meals[0])
@@ -53,6 +71,7 @@ const renderFoodInfo = food => {
         `;
     const listItem = listOfInstruction(food.strInstructions);
     document.getElementById("list").appendChild(listItem);
+
 }
 
 // convert Introduction String to li
